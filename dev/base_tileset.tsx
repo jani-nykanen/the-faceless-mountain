0db22db273f3d01d056726237e5a483a9279f0bf
Jani Nykänen <jnyknn@gmail.com>
2018-11-03 15:20:13 +0200

    Tileset & map 1 beginnings

---
+++ dev/base_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="64" tileheight="64" tilecount="48" columns="16">
+ <image source="../monogame-experiment/Assets/Bitmaps/tileset.png" width="1024" height="192"/>
+</tileset>
