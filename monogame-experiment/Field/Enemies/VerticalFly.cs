9b3b508b9602ea59cdb2e97eed29382d683c1aad
Jani Nykänen <jnyknn@gmail.com>
2018-11-11 16:28:41 +0200

    First two enemy sprites

--- monogame-experiment/Field/Enemies/VerticalFly.cs
+++ monogame-experiment/Field/Enemies/VerticalFly.cs
@@ -20,7 +20,9 @@ namespace monogame_experiment.Desktop.Field.Enemies
         // Animate
         protected override void Animate(float tm)
         {
-            spr.Animate(1, 0, 0, 0, tm);
+            const float ANIM_SPEED = 6.0f;
+
+            spr.Animate(1, 0, 3, ANIM_SPEED, tm);
         }
 
 
